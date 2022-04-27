33c92ee272df6dd5506010048551ef99f77c1458
sean-pwd <sean@powerfulwebdesign.com.au>
2022-04-26 23:51:02 -0400

    fix: lesson complete

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,1 +1,26 @@
 import Fuse from 'fuse.js/dist/fuse.basic.esm.js'
+
+const searchInput = document.getElementById('js-searchInput');
+const searchResults = document.getElementById('js-searchResults');
+
+
+// ***********************
+// search params function
+
+function params(name) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(name)
+}
+
+// ***********************
+// check to see if a search has taken place
+const searchQuery = params('q');
+if (searchQuery) {
+  // add the query to search input
+  searchInput.value = searchQuery;
+  searchResults.innerHTML = 'Loading...';
+  // run the search
+} else { // no search query
+  searchResults.innerHTML = 'Please input your search into the search box above.';
+}
