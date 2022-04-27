8d2c9a41825801309a12d7ab08ce6d00ecdfbd04
sean-pwd <sean@powerfulwebdesign.com.au>
2022-04-27 01:28:09 -0400

    fix: lesson complete

--- assets/js/search.js
+++ assets/js/search.js
@@ -24,3 +24,15 @@ if (searchQuery) {
 } else { // no search query
   searchResults.innerHTML = 'Please input your search into the search box above.';
 }
+
+const searchIndex = `
+  {{ $index := newScratch }}
+  {{ $pages := .Site.RegularPages }}
+  {{ $pages = where $pages "Params.private" "!=" "true" }}
+  {{ $index.Set "index" slice }}
+  {{ range $pages }}
+    {{ $index.Add "index" (dict "title" .Title "permalink" .Permalink "images" .Params.images "content" .Plain "summary" .Summary "companies" .Params.companies "species" .Params.species )}}
+  {{ end }}
+  {{ $index.Get "index" | jsonify }}
+ `
+console.log(JSON.stringify(searchIndex));
