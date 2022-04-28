119bfc09a795a162a61e0e3eec16086df47691f9
sean-pwd <sean@powerfulwebdesign.com.au>
2022-04-27 21:09:24 -0400

    fix: in progress

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,28 +3,6 @@ import Fuse from 'fuse.js/dist/fuse.basic.esm.js'
 const searchInput = document.getElementById('js-searchInput');
 const searchResults = document.getElementById('js-searchResults');
 
-
-// ***********************
-// search params function
-
-function params(name) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  return urlParams.get(name)
-}
-
-// ***********************
-// check to see if a search has taken place
-const searchQuery = params('q');
-if (searchQuery) {
-  // add the query to search input
-  searchInput.value = searchQuery;
-  searchResults.innerHTML = 'Loading...';
-  // run the search
-} else { // no search query
-  searchResults.innerHTML = 'Please input your search into the search box above.';
-}
-
 const searchIndex = `
   {{ $index := newScratch }}
   {{ $pages := .Site.RegularPages }}
@@ -36,3 +14,61 @@ const searchIndex = `
   {{ $index.Get "index" | jsonify }}
  `
 console.log(JSON.stringify(searchIndex));
+
+// ***********************
+// search params function
+//
+function params(name) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(name)
+}
+
+// ***********************
+// check to see if a search has taken place
+//
+const searchQuery = params('q');
+if (searchQuery) {
+  // add the query to search input
+  searchInput.value = searchQuery;
+  searchResults.innerHTML = 'Loading...';
+  // run the search
+  search(searchIndex);
+} else { // no search query
+  searchResults.innerHTML = 'Please input your search into the search box above.';
+}
+
+
+// ***********************
+// run search
+//
+function search(data) {
+  const options = {
+    // isCaseSensitive: false,
+    // includeScore: false,
+    // shouldSort: true,
+    // includeMatches: false,
+    findAllMatches: true,
+    minMatchCharLength: 2,
+    // location: 0,
+    threshold: 0.4,
+    // distance: 100,
+    // useExtendedSearch: false,
+    ignoreLocation: true,
+    // ignoreFieldNorm: false,
+    // fieldNormWeight: 1,
+    keys: [
+      "title", // default weight 1
+      {name: "summary", weight: 0.8},
+      { name: "content", weight: 0.6 },
+      { name: "companies", weight: 0.4},
+      { name: "species", weight: 0.4},
+    ]
+  };
+  // create new fuse instance
+  const fuse = new Fuse(data, options);
+  // return results
+  const results = fuse.search(searchQuery)
+
+  //console.log(results);
+}
